488b10754d20b0871ad104205ce4e551018ec423
JFBranch <74365111+Art3Labs@users.noreply.github.com>
2024-07-14 19:53:56 +0200

    v2 certificado con signin

--- pages/certificate.js
+++ pages/certificate.js
@@ -15,31 +15,32 @@ export default function Index() {
 
       <section className='bg-gradient-to-b from-indigo-900 to-gray-400 '>
         <div className='p-4 mx-auto max-w-screen-xl lg:pt-10 pb-10'>
-          <div>
-            <img src='/images/Certificate.png' className='hover:shadow-lg' alt='Certificate' />
+          <div >
+            <img src='/images/Certificate.png' className='mx-auto hover:shadow-lg' alt='Certificate' />
           </div>
-          <Link href='/check-in'>
-            <article className='p-4 bg-white rounded-lg  border-gray-200 shadow-md mt-10 mx-auto' style={{ maxWidth: "550px" }}>
-              <div className='flex justify-between items-center mb-5 text-gray-500'>
-                <span className='text-sm'></span>
-              </div>
-              <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
-                Suggested books to keep your learning path
-              </h2>
-              <div className='flex justify-between items-center'>
-                <div className='flex-1'>
-                  <ul className='list-reset mb-6'>
-                    <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                      <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Measure what matters 	by John Doerr, buy from Amazon</div>
-                    </li>
-                    <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                      <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Marketing management 	by Philip Kotler, buy it from Thalia </div>
-                    </li>
-                  </ul>
+          <div className='grid gap-8 lg:grid-cols-2 sm:grid-cols-2 mx-auto content-center justify-center' style={{ maxWidth: "1200px" }} >
+            <Link href='/'>
+              <article className='p-4 bg-white rounded-lg  border-gray-200 shadow-md mt-10 mx-auto' style={{ maxWidth: "550px" }}>
+                <div className='flex justify-between items-center mb-5 text-gray-500'>
+                  <span className='text-sm'></span>
                 </div>
-           
-              </div>
-              <div className='flex justify-between items-center'>
+                <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                  Books to keep your learning path
+                </h2>
+                <div className='flex justify-between items-center'>
+                  <div className='flex-1'>
+                    <ul className='list-reset mb-6'>
+                      <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
+                        <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Measure what matters 	by John Doerr, buy from Amazon</div>
+                      </li>
+                      <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
+                        <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Marketing management 	by Philip Kotler, buy it from Thalia </div>
+                      </li>
+                    </ul>
+                  </div>
+
+                </div>
+                <div className='flex justify-between items-center'>
                   <div className='flex items-center space-x-4'>
                   </div>
                   <div className='inline-flex items-center font-medium text-primary-600 hover:underline'>
@@ -56,89 +57,33 @@ export default function Index() {
                     </svg>
                   </div>
                 </div>
-            </article>
-          </Link>
-          <Link href='/check-in'>
-            <article className='p-4 bg-white rounded-lg  border-gray-200 shadow-md mt-10 mx-auto' style={{ maxWidth: "550px" }}>
-              <div className='flex justify-between items-center mb-5 text-gray-500'>
-                <span className='text-sm'></span>
-              </div>
-              <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
-                Suggested courses to keep your learning path
-              </h2>
-              <div className='flex justify-between items-center'>
-                <div className='flex-1'>
-                  <ul className='list-reset mb-6'>
-                    <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                      <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Course 1 on LinkedIn learning, get one month suscription free with our invitation. </div>
-                    </li>
-                    <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                      <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Course 2 on Coursera, get two weeks suscription free with our invitation.  </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </article>
-          </Link>
-        </div>
-
-        <Footer></Footer>
-      </section>
-
-    </>
-  );
-}
-
-/*
-  <div className='flex justify-center pt- '>
-          <div className="grid grid-cols-3 gap-2 my-6">
-            <div>
-              <Link href="/sobre-nosotros">
-                <button type="button" className="inline-block px-3 py-3 bg-indigo-600 text-white font-medium text-base leading-tight  rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" style={{ maxWidth: "200px" }}>
-                  Conócenos
-                </button>
-
-              </Link>
-            </div>
-            <div>
-              <Link href="/contacto">
-                <button type="button" className="inline-block px-3 py-3 bg-indigo-600 text-white font-medium text-base leading-tight  rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" style={{ maxWidth: "200px" }}>
-                  Contáctanos
-                </button>
-              </Link>
-            </div>
-
-            <div>
-              <Link href="https://mercado.gdosolar.es/">
-                <button type="button" className="inline-block mx-2 px-3 py-3 bg-cyan-500 text-white font-medium text-base leading-tight  rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" style={{ maxWidth: "200px" }}>
-                  Mercado
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-
-
-Envía tus órdenes de compra y venta a registro@gdosolar.es, las publicamos de forma anónima en nuestro mercado. Te ponemos en contacto con la contraparte.
-
-            <Link href='/financiacion-verde'>
-              <article className='p-4 bg-white rounded-lg border-gray-200 shadow-md' style={{ maxWidth: "450px" }}>
+              </article>
+            </Link>
+            <Link href='/'>
+              <article className='p-4 bg-white rounded-lg  border-gray-200 shadow-md mt-10 mx-auto' style={{ maxWidth: "550px" }}>
                 <div className='flex justify-between items-center mb-5 text-gray-500'>
                   <span className='text-sm'></span>
                 </div>
                 <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
-                  Financiación verde
+                  Courses to keep your learning path
                 </h2>
-                <p className='mb-5 font-light text-gray-500 '>
-                  
-                ¿Estás construyendo un nueva instalación de energías renovables? Descubre una nueva fuente de financiación.
-                  Vende tus garantías de origen y consigue un valor añadido a tu producción.
-                </p>
+                <div className='flex justify-between items-center'>
+                  <div className='flex-1'>
+                    <ul className='list-reset mb-6'>
+                      <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
+                        <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Course 1 on LinkedIn learning, get one month suscription free with our invitation. </div>
+                      </li>
+                      <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
+                        <div className='font-light no-underline hover:underline text-gray-800 hover:text-green-500'>Course 2 on Coursera, get two weeks suscription free with our invitation.  </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
                 <div className='flex justify-between items-center'>
                   <div className='flex items-center space-x-4'>
                   </div>
                   <div className='inline-flex items-center font-medium text-primary-600 hover:underline'>
-                    Preventa de certificados
+                    See more courses recommendations
                     <svg
                       className='ml-2 w-4 h-4'
                       fill='currentColor'
@@ -151,34 +96,77 @@ Envía tus órdenes de compra y venta a registro@gdosolar.es, las publicamos de 
                     </svg>
                   </div>
                 </div>
-                <div className="background-container ">
-                  <div className="mobile-image">
-                    <img src="/greenfinance.png" alt="Background Image" />
-                  </div>
-                  <div className="video-container">
-                    <VideoBackground3 />
-                  </div>
-                </div>
               </article>
             </Link>
+          </div>
+          <article className='p-4 bg-white rounded-lg  border-gray-200 shadow-md mt-10 mx-auto' style={{ maxWidth: "450px" }}>
+            <div className="mt-5 mb-5 sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign in to your account
+              </h2>
+              <p className="mt-2 mb-4  text-center text-sm text-gray-500">
+                Not a member?{' '}
+                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                  Create your account, it is free!
+                </a>
+              </p>
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
 
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      Password
+                    </label>
+                    <div className="text-sm">
+                      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                        Forgot password?
+                      </a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
 
-<div className='mt-8 mb-2- mx-auto' style={{ maxWidth: "900px" }} >
-              <Link href='/plataforma'>
-                <article className='px-4 py-4 bg-gray-100 rounded-lg border border-gray-200 shadow-md' style={{ maxWidth: "970px" }}>
-                  <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
-                    Oportunidad de inversión en GdOsolar
-                  </h2>
-                  <h3 className='mb-2 text-xl font-bold tracking-tight text-gray-700'>
-                    Buscamos socios de la industria energética: productores, comercializadoras, distribuidoras etc.
-                  </h3>
-                  <p className=' font-light text-gray-500 '>
-                    Unimos la fuerza del sol con una innovadora plataforma de negociación de certificados de energía renovable, iluminando el camino hacia un futuro sostenible y rentable para todos los inversores comprometidos con la transición energética.
-                  </p>
-                </article>
-              </Link>
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+
             </div>
-Video solution for hydration
+          </article>
+        </div>
 
-import dynamic from 'next/dynamic'
-const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });*/
+        <Footer></Footer>
+      </section>
+
+    </>
+  );
+}
